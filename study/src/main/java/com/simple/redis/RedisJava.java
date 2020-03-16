f99c8a52eaab4365a8750644b83812c103e113e5
wangyongsd2011@163.com <wangyongsd2011@163.com>
2020-03-16 08:51:03 +0800

    提交文档

--- study/src/main/java/com/simple/redis/RedisJava.java
+++ study/src/main/java/com/simple/redis/RedisJava.java
@@ -1,10 +1,14 @@
 package com.simple.redis;
 
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
 import redis.clients.jedis.Jedis;
 
 public class RedisJava {
 	
-	public void testResdisDemo(Jedis jedis ) {
+	public void testResdisHashDemo(Jedis jedis ) {
 		   //设置 redis 字符串数据
         jedis.set("runoobkey", "www.runoob.com");
         // 获取存储的数据并输出
@@ -12,17 +16,56 @@ public class RedisJava {
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		// Jedis jedis = new Jedis("localhost");
-		
+			
 		RedisJava redisJava = new RedisJava();
 		Jedis jedis = new Jedis("127.0.0.1");
-		System.out.println("连接成功");
-		// 查看服务是否运行
-		System.out.println("服务正在运行: " + jedis.ping());
+		//redis 连接信息
+		{
+			System.out.println("连接成功");
+			// 查看服务是否运行
+			System.out.println("服务正在运行: " + jedis.ping());
+		}
 		
-		redisJava.testResdisDemo(jedis);
-
+		
+		//demohash
+		redisJava.testResdisHashDemo(jedis);
+		
+		//demo list
+		redisJava.testResdisListDemo(jedis);
+		
+		//key 
+		System.out.println("redis keys");
+		redisJava.testResdisKeysDemo(jedis);
+		jedis.del("*");
+		redisJava.testResdisKeysDemo(jedis);
+	}
+	
+	//redis list
+	public void testResdisListDemo(Jedis jedis ) {
+		
+		jedis.ltrim("site-list", -1L,0L); 
+		
+		//存储数据到列表中
+	    jedis.lpush("site-list", "Runoob");
+	    jedis.lpush("site-list", "Google");
+	    jedis.lpush("site-list", "Taobao");
+	    
+	    // 获取存储的数据并输出
+	    List<String> list = jedis.lrange("site-list", 0 ,jedis.llen("site-list"));
+	    for(int i=0; i<=list.size()-1; i++) {
+	        System.out.println("列表项为: "+list.get(i));
+	    }
+	}
+	
+	//keys
+	public void testResdisKeysDemo(Jedis jedis ) {
+		// 获取数据并输出
+        Set<String> keys = jedis.keys("*"); 
+        Iterator<String> it=keys.iterator() ;   
+        while(it.hasNext()){   
+            String key = it.next();   
+            System.out.println(key);   
+        }
 	}
 
 }
